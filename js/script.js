c3ea0e802e026a82b6051397a9dbfe2e5af6058e
shportaklev <shportaklev@mail.ru>
2020-04-08 16:39:25 +0500

    finish services 1

--- js/script.js
+++ js/script.js
@@ -1,50 +1,40 @@
 window.onload = function () {
-  const brendsItems = document.querySelectorAll(".brends__item");
-  const brendsItemsCount = brendsItems.length;
-  const brendsFirstSixItems = document.querySelectorAll(
-    ".brends__item:nth-child(-n+6)"
-  );
-
+  const brendsItems = document.querySelector(".brends__items");
+  const brends = document.querySelector(".brends");
   const brendsMoreBtn = document.querySelector(".brends__more-btn");
-  let brendsItemsDefault = 8;
-  const brendsLastItems = document.querySelectorAll(
-    `.brends__item:nth-last-child(-n+ ${brendsItemsCount - brendsItemsDefault})`
-  );
+  const brendsMobile = document.querySelector(".brends-mobile");
+  const btnLinkBefore = document.querySelector(".brend-slide__btn-link");
+
   brendsMoreBtn.addEventListener("click", function () {
     if (brendsMoreBtn.classList.contains("more-btn--open")) {
       brendsMoreBtn.classList.remove("more-btn--open");
-      brendsItems.forEach((item) => {
-        item.classList.remove("brends__item--hidden");
-      });
+      brends.classList.remove("brends--expanded");
+      brendsItems.classList.remove("brends__items--expanded");
     } else {
-      let countNodes = brendsItems.length;
-      brendsLastItems.forEach((item) => {
-        item.classList.add("brends__item--hidden");
-      });
       brendsMoreBtn.classList.add("more-btn--open");
+      brends.classList.add("brends--expanded");
+      brendsItems.classList.add("brends__items--expanded");
     }
   });
 
+
   var swiper = new Swiper(".swiper-container", {
     spaceBetween: 0,
-    width: 240,
+    width: 256,
 
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
   });
-  let brends = document.querySelector(".brends");
-  let brendsMobile = document.querySelector(".brends-mobile");
+
+  
   if (window.innerWidth < 350) {
     brends.classList.add("display-none");
     brendsMobile.classList.remove("display-none");
-  } else if (window.innerWidth > 350 && window.innerWidth <= 768) {
-    brendsItemsDefault = 6;
-    console.log(brendsItemsDefault);
+  } else {
     brends.classList.remove("display-none");
     brendsMobile.classList.add("display-none");
   }
-  console.log(window.innerWidth);
-  // console.log(brendsItemsCount);
+  console.log(btnLinkBefore);
 };
