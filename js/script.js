1ba6a14895d2d7eb9214b07ddef6f0be715d231f
programmwriter <shportaklev@mail.ru>
2020-04-08 22:28:11 +0500

    service finish

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 window.onload = function () {
   const brendsItems = document.querySelector(".brends__items");
   const brends = document.querySelector(".brends");
-  const brendsMoreBtn = document.querySelector(".brends__more-btn");
   const brendsMobile = document.querySelector(".brends-mobile");
-  const btnLinkBefore = document.querySelector(".brend-slide__btn-link");
   const moreBtnOpen = "more-btn--open";
   const brendsExpanded = "brends--expanded";
   const brendsItemsExpanded = "brends__items--expanded";
@@ -35,29 +33,6 @@ window.onload = function () {
     });
   });
 
-  // function moreBtnClick(event) {
-  //   if (event.target.classList.contains(moreBtnOpen)) {
-  //     event.target.classList.remove(moreBtnOpen);
-  //     brends.classList.remove(brendsExpanded);
-  //     brendsItems.classList.remove(brendsItemsExpanded);
-  //   } else {
-  //     event.target.classList.add(moreBtnOpen);
-  //     brends.classList.add(brendsExpanded);
-  //     brendsItems.classList.add(brendsItemsExpanded);
-  //   }
-  // }
-
-  // brendsMoreBtn.addEventListener("click", function (event) {
-  //   if (brendsMoreBtn.classList.contains(moreBtnOpen)) {
-  //     brendsMoreBtn.classList.remove(moreBtnOpen);
-  //     brends.classList.remove(brendsExpanded);
-  //     brendsItems.classList.remove(brendsItemsExpanded);
-  //   } else {
-  //     brendsMoreBtn.classList.add(moreBtnOpen);
-  //     brends.classList.add(brendsExpanded);
-  //     brendsItems.classList.add(brendsItemsExpanded);
-  //   }
-  // });
 
   var swiper = new Swiper(".swiper-container", {
     spaceBetween: 0,
